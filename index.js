c606d8797a461eedf29137b787595b6e5703d823
1mpact100 <sivanov090909@gmail.com>
2026-06-17 21:11:17 +0300

    lab5

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
 
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 const PORT = process.env.PORT || 10000;
 
-app.use(express.text({ type: '*/*' }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'x-test,ngrok-skip-browser-warning,Content-Type,Accept,Access-Control-Allow-Headers'
-  );
-  next();
+app.get('/login', (req, res) => {
+  res.type('text/plain; charset=utf-8');
+  res.send('1160491');
 });
 
-app.all('/result4/', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+app.post('/decypher', upload.fields([
+  { name: 'key', maxCount: 1 },
+  { name: 'secret', maxCount: 1 }
+]), (req, res) => {
+  try {
+    const key = req.files.key[0].buffer.toString('utf8');
+    const secretBuffer = req.files.secret[0].buffer;
 
-  res.end(JSON.stringify({
-    message: '1160491',
-    'x-result': req.get('x-test') || '',
-    'x-body': req.body || ''
-  }));
+    let encrypted;
+
+    const secretText = secretBuffer.toString('utf8').trim();
+
+    if (/^[A-Za-z0-9+/=\r\n]+$/.test(secretText)) {
+      encrypted = Buffer.from(secretText, 'base64');
+    } else {
+      encrypted = secretBuffer;
+    }
+
+    const decrypted = crypto.privateDecrypt(
+      {
+        key,
+        padding: crypto.constants.RSA_PKCS1_PADDING
+      },
+      encrypted
+    );
+
+    res.type('text/plain; charset=utf-8');
+    res.send(decrypted.toString('utf8'));
+  } catch (err) {
+    res.status(500).type('text/plain; charset=utf-8');
+    res.send('decryption error');
+  }
 });
 
 app.listen(PORT, '0.0.0.0');
